766c987b2349987babc3acb4a5b8471efd81dc6b
Soungbin Cho <soungbinc@gmail.com>
2024-06-18 02:47:21 -0700

    Login_Create_UI

---
+++ src/screens/Login.js
@@ -0,0 +1,86 @@
+import {
+    faFacebookSquare,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useForm } from "react-hook-form"
+import styled from "styled-components"
+import AuthLayout from "../components/auth/AuthLayout"
+import BottomBox from "../components/auth/BottomBox"
+import Button from "../components/auth/Button"
+import FormBox from "../components/auth/FormBox"
+import FormError from "../components/auth/FormError"
+import Input from "../components/auth/input"
+import Separator from "../components/auth/Separator"
+import PageTitle from "../components/PageTitle"
+import routes from "../routes"
+
+const FacebookLogin = styled.div`
+    color: #385285;
+    span {
+        margin-left: 10px;
+        font-weight: 600;
+    }
+`
+
+function Login() {
+    const { register, handleSubmit, errors, formState } = useForm({
+        mode: "onChange",
+    })
+    const onSubmitValid = (data) => {
+        //console.log(data);
+    }
+    return (
+        <AuthLayout>
+            <PageTitle title="Login" />
+            <FormBox>
+                <div>
+                    <FontAwesomeIcon icon={faInstagram} size="3x" />
+                </div>
+                <form onSubmit={handleSubmit(onSubmitValid)}>
+                    <Input
+                        {...register("username", {
+                            required: "Username is required",
+                            minLength: {
+                                value: 5,
+                                message:
+                                    "Username should be longer than 5 chars.",
+                            },
+                        })}
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        hasError={Boolean(errors?.username?.message)}
+                    />
+                    <FormError message={errors?.username?.message} />
+                    <Input
+                        {...register("password", {
+                            required: "Password is required.",
+                        })}
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        hasError={Boolean(errors?.password?.message)}
+                    />
+                    <FormError message={errors?.password?.message} />
+                    <Button
+                        type="submit"
+                        value="Log in"
+                        disabled={!formState.isValid}
+                    />
+                </form>
+                <Separator />
+                <FacebookLogin>
+                    <FontAwesomeIcon icon={faFacebookSquare} />
+                    <span>Log in with Facebook</span>
+                </FacebookLogin>
+            </FormBox>
+            <BottomBox
+                cta="Don't have an account?"
+                linkText="Sign up"
+                link={routes.signUp}
+            />
+        </AuthLayout>
+    )
+}
+export default Login
